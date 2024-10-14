8936b0dfe8a29224f4a8bb004cc5a4d1e676ed50
Dustin Abhishek <davidpiyush225@gmail.com>
2024-10-14 14:48:40 +0530

    fix window reference

--- app/_components/ReferInfo.jsx
+++ app/_components/ReferInfo.jsx
@@ -1,45 +1,100 @@
+// "use client";
+// import { useState } from "react";
+
+// import "@/app/_styles/style.css";
+// import { handleCopy } from "../_utlis/CopyToClipboard";
+// import { Toaster } from "react-hot-toast";
+
+
+// function ReferInfo({ referCode, referUserCount, referBalance }) {
+//   // const [token ,setToken] = useState([0])
+
+//   const baseUrl = window?.location.origin;
+//   //token.length>0 ? token: ""
+
+//   const referLink = referCode ? `${baseUrl}/referral?code=${referCode}` : "";
+
+//   return (
+//     <section id="referralInfo" class="section-el ">
+//       <div class="infoBox ">
+//         <h3>Your Referral Link</h3>
+//         <input
+//           type="text"
+//           id="referralLink"
+//           class="input"
+//           readOnly=""
+//           defaultValue={referLink}
+//         />
+//         <button
+//           id="copyLinkButton button"
+//           onClick={() => handleCopy(referLink)}
+//         >
+//           Copy Link
+//         </button>
+//       </div>
+//       <div class="infoBox">
+//         <h3>Your Referrals</h3>
+//         <p>
+//           <span id="referralCount">{referUserCount || 0}</span> Friends Referred
+//         </p>
+//       </div>
+//       <div class="infoBox">
+//         <h3>Earnings From Referrals</h3>
+//         <p>
+//           <span id="referralEarnings">{referBalance || 0}</span> Points Earned
+//         </p>
+//       </div>
+//       <Toaster />
+//     </section>
+//   );
+// }
+
+// export default ReferInfo;
+
+
 "use client";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 import "@/app/_styles/style.css";
-import generateRandomToken from "../_utlis/tokenGenerator";
 import { handleCopy } from "../_utlis/CopyToClipboard";
 import { Toaster } from "react-hot-toast";
 
-
 function ReferInfo({ referCode, referUserCount, referBalance }) {
-  // const [token ,setToken] = useState([0])
+  const [baseUrl, setBaseUrl] = useState("");
+  const [referLink, setReferLink] = useState("");
 
-  const baseUrl = window.location.origin;
-  //token.length>0 ? token: ""
+  useEffect(() => {
+    // Set baseUrl only in the client environment
+    const url = window.location.origin;
+    setBaseUrl(url);
 
-  const referLink = referCode ? `${baseUrl}/referral?code=${referCode}` : "";
+    if (referCode) {
+      setReferLink(`${url}/referral?code=${referCode}`);
+    }
+  }, [referCode]);
 
   return (
-    <section id="referralInfo" class="section-el ">
-      <div class="infoBox ">
+    <section id="referralInfo" className="section-el">
+      <div className="infoBox">
         <h3>Your Referral Link</h3>
         <input
           type="text"
           id="referralLink"
-          class="input"
-          readOnly=""
-          defaultValue={referLink}
+          className="input"
+          readOnly
+          value={referLink} // Use value instead of defaultValue
         />
-        <button
-          id="copyLinkButton button"
-          onClick={() => handleCopy(referLink)}
-        >
+        <button id="copyLinkButton" onClick={() => handleCopy(referLink)}>
           Copy Link
         </button>
       </div>
-      <div class="infoBox">
+      <div className="infoBox">
         <h3>Your Referrals</h3>
         <p>
           <span id="referralCount">{referUserCount || 0}</span> Friends Referred
         </p>
       </div>
-      <div class="infoBox">
+      <div className="infoBox">
         <h3>Earnings From Referrals</h3>
         <p>
           <span id="referralEarnings">{referBalance || 0}</span> Points Earned
